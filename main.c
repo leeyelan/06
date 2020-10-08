8c6f17690347d6ecbaf75f826780d748a7c45aae
leeyelan <yr00868@sookmyung.ac.kr>
2020-10-08 15:51:14 +0900

    2nd

--- main.c
+++ main.c
@@ -3,18 +3,34 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void print_star(){
-	
-	int i;
-	for(i=0; i<10; i++)
-	printf("*");
+
+int sumTwo(int a, int b){
+	return(a+b);
 }
-	int main(void) {
+
+int square(int n){
+	return(n*n);
+}
+
+int get_max(int x, int y){
+	if(x>y)
+	   return x;
+	else
+	return(y);
+}
+
+
+int main(int argc, char*argv[]){
 	
-	print_star();
-	print_star();
-	print_star();
-		
+	printf("sumTwo : %d\n",sumTwo(1,2));
+	printf("square : %d\n",square(5));
+	printf("get_max : %d\n", get_max(10,30));
 	
 	return 0;
 }
+
+
+
+
+	
+
